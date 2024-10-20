447795e9f2048a5a7bfe482dd78782905c4cf758
Kroem Vandy <kroem123vandy@gmail.com>
2024-10-20 15:35:51 +0700

    done reports

---
+++ resources/views/dashboard/report/list-of-name.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.templates.att.master')
+@push('CSS')
+@endpush
+
+@section('Content')
+
+<div class="p-4">
+    <div class="p-4 shadow-md bg-white rounded-lg">
+        <h1 class="font-moul text-gray-800 text-lg text-center">បញ្ជីរាយនាម</h1>
+        <hr>
+    </div>
+</div>
+
+@endsection
+
+@push('JS')
+@endpush
