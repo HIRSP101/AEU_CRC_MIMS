6f3d7f7f08e224b840bce8179855dcc5c3ab3346
viseth <simviseth509@gmail.com>
2025-03-11 09:24:14 +0700

    alert

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div id="sidebar" class="top-0 left-0 border shadow bg-red-600">
+<div id="sidebar" class="top-0 left-0 border shadow bg-white w-64">
     <div class="flex flex-col flex-1 overflow-y-auto">
         <nav class="flex-1 px-2 py-4">
 
@@ -11,9 +11,8 @@
                         class="flex items-center font-siemreap px-4 py-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
                         <img width="23" height="23"
                             src="https://img.icons8.com/external-glyph-zulfa-mahendra/48/external-building-nature-and-ecology-2-glyph-zulfa-mahendra-3.png"
-                            alt="external-building-nature-and-ecology-2-glyph-zulfa-mahendra-3"
-                            class="invert brightness-200" />
-                        <span class="module-content ml-2 text-white">
+                            alt="external-building-nature-and-ecology-2-glyph-zulfa-mahendra-3" />
+                        <span class="module-content ml-2">
                             សាខា
                         </span>
                     </a>
@@ -21,21 +20,20 @@
                         class="flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
                         <img width="23" height="23"
                             src="https://img.icons8.com/external-gradak-royyan-wijaya/24/external-building-gradak-medical-solidarity-gradak-royyan-wijaya.png"
-                            alt="external-building-gradak-medical-solidarity-gradak-royyan-wijaya"
-                            class="invert brightness-200" />
-                        <span class="module-content ml-2 text-white">
+                            alt="external-building-gradak-medical-solidarity-gradak-royyan-wijaya" />
+                        <span class="module-content ml-2">
                             គ្រឹះស្ថានឧត្តមសិក្សា
                         </span>
                     </a>
                     <a href="{{route('document')}}"
-                        class="group flex items-center font-siemreap px-4 py-2 mt-2 font-semibold hover:bg-white hover:text-red-600 rounded">
+                        class="group flex items-center font-siemreap px-4 py-2 mt-2 font-semibold hover:bg-red-400 rounded">
                         <img width="23" height="23" src="https://img.icons8.com/ios-filled/50/ratings.png" alt="ratings"
                             class="transition-all duration-200 filter-white group-hover:filter-red" />
-                        <span class="module-content ml-2 text-white group-hover:text-red-600">
+                        <span class="module-content ml-2">
                             ឯកសារដ្ឋបាល
                         </span>
                     </a>
-                    <style>
+                    {{-- <style>
                         .filter-white {
                             filter: invert(100%) brightness(200%);
                         }
@@ -43,67 +41,65 @@
                         .group:hover .filter-white {
                             filter: invert(18%) sepia(98%) saturate(7481%) hue-rotate(358deg) brightness(96%) contrast(117%);
                         }
-                    </style>
+                    </style> --}}
                 @endif
                 <a href="" id="subModule-add"
                     class="flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
                     <img width="23" height="23"
                         src="https://img.icons8.com/ios-filled/50/add-user-group-man-man--v2.png"
-                        alt="add-user-group-man-man--v2" class="invert brightness-200" />
+                        alt="add-user-group-man-man--v2" />
                     <span
-                        class="module-content ml-2 flex-1 text-left rtl:text-right whitespace-nowrap text-white">បញ្ចូលសាមាជិក</span>
+                        class="module-content ml-2 flex-1 text-left rtl:text-right whitespace-nowrap">បញ្ចូលសាមាជិក</span>
                 </a>
                 <div id="dropdown-add" class="ml-5 dropdown_entry hidden">
                     <a href="{{ route('import') }}"
                         class=" module-content flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
-                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png" alt="add--v1"
-                            class="invert brightness-200" />
-                        <span class="text-white"> បញ្ជូលជា File</span>
+                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png"
+                            alt="add--v1" />
+                        <span> បញ្ជូលជា File</span>
                     </a>
                     <a href="{{ route('createmember') }}"
                         class="module-content flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
-                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png" alt="add--v1"
-                            class="invert brightness-200" />
-                        <span class="text-white"> បញ្ជូលតាម Form</span>
+                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png"
+                            alt="add--v1" />
+                        <span> បញ្ជូលតាម Form</span>
                     </a>
                 </div>
                 <a href="" id="subModule-create"
                     class="flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
                     <img width="23" height="23"
                         src="https://img.icons8.com/ios-filled/50/add-user-group-man-man--v2.png"
-                        alt="add-user-group-man-man--v2" class="invert brightness-200" />
-                    <span
-                        class="module-content ml-2 flex-1 text-left rtl:text-right whitespace-nowrap text-white">បង្កើត</span>
+                        alt="add-user-group-man-man--v2" />
+                    <span class="module-content ml-2 flex-1 text-left rtl:text-right whitespace-nowrap">បង្កើត</span>
                 </a>
                 <div id="dropdown-create" class="ml-5 dropdown_entry hidden">
                     <a href="{{ route('userroles') }}"
                         class=" module-content flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
-                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png" alt="add--v1"
-                            class="invert brightness-200" />
-                        <span class="text-white">អ្នកប្រើប្រាស់</span>
+                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png"
+                            alt="add--v1" />
+                        <span>អ្នកប្រើប្រាស់</span>
                     </a>
                     <a href="{{ route('createdistrict') }}"
                         class="module-content flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
-                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png" alt="add--v1"
-                            class="invert brightness-200" />
-                        <span class="text-white"> ស្រុក/ក្រុង</span>
+                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png"
+                            alt="add--v1" />
+                        <span> ស្រុក/ក្រុង</span>
                     </a>
                     <a href="{{ route('createschool') }}"
                         class="module-content flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
-                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png" alt="add--v1"
-                            class="invert brightness-200" />
-                        <span class="text-white"> គ្រឹះស្ថានសិក្សា</span>
+                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png"
+                            alt="add--v1" />
+                        <span> គ្រឹះស្ថានសិក្សា</span>
                     </a>
                 </div>
                 <a href="{{route('report')}}"
                     class="flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
-                    <img width="23" height="23" src="https://img.icons8.com/ios-filled/50/ratings.png" alt="ratings"
-                        class="invert brightness-200" />
-                    <span class="module-content ml-2 text-white">
+                    <img width="23" height="23" src="https://img.icons8.com/ios-filled/50/ratings.png" alt="ratings" />
+                    <span class="module-content ml-2">
                         របាយការណ៍
                     </span>
                 </a>
-                <a href="" id="subModule-expire"
+                {{-- <a href="" id="subModule-expire"
                     class="flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
                     <img width="23" height="23"
                         src="https://img.icons8.com/ios-filled/50/add-user-group-man-man--v2.png"
@@ -112,21 +108,33 @@
                         class="module-content ml-2 flex-1 text-left rtl:text-right whitespace-nowrap text-white">ផុតកំណត់</span>
                     <span id="total-expired-notification"
                         class="bg-red-500 text-white rounded-full px-2 hidden">0</span>
+                </a> --}}
+                <a href="" id="subModule-expire"
+                    class="flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
+                    <img width="23" height="23"
+                        src="https://img.icons8.com/ios-filled/50/add-user-group-man-man--v2.png"
+                        alt="add-user-group-man-man--v2" />
+                    <span class="module-content ml-2 flex-1 text-left rtl:text-right whitespace-nowrap">
+                        ផុតកំណត់
+                    </span>
+                    <span id="total-expired-notification"
+                        class="bg-red-500 text-white rounded-full px-2 hidden">0</span>
                 </a>
+
                 <div id="dropdown-expire" class="ml-5 dropdown_entry hidden">
                     <a href="{{ route('expire') }}"
                         class=" module-content flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
-                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png" alt="add--v1"
-                            class="invert brightness-200" />
-                        <span class="text-white"> អនុវិទ្យាល័យ/វិទ្យាល័យ</span>
+                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png"
+                            alt="add--v1" />
+                        <span> វិទ្យាល័យ</span>
                         <span id="expired-member-highschool"
                             class="bg-red-500 text-white rounded-full px-2 hidden">0</span>
                     </a>
                     <a href="{{ route('institute_ex') }}"
                         class="module-content flex items-center font-siemreap px-4 py-2 mt-2 text-gray-800 font-semibold hover:bg-red-400 rounded">
-                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png" alt="add--v1"
-                            class="invert brightness-200" />
-                        <span class="text-white"> សាកលវិទ្យាល័យ</span>
+                        <img width="22" height="22" src="https://img.icons8.com/ios-glyphs/30/add--v1.png"
+                            alt="add--v1" />
+                        <span> សាកលវិទ្យាល័យ</span>
                         <span id="expired-member-institute"
                             class="bg-red-500 text-white rounded-full px-2 hidden">0</span>
                     </a>
@@ -153,6 +161,19 @@
         })
 
         document.addEventListener("DOMContentLoaded", function () {
+
+            let notification = document.getElementById("total-expired-notification");
+            // let notiSchool = document.getElementById("expired-member-highschool");
+            // let notiInsitute = document.getElementById("expired-member-institute");
+
+            // if (localStorage.getItem("notificationClicked") === "true") {
+            //     notification.style.display = "none";
+            // }
+            // document.getElementById("subModule-expire").addEventListener("click", function () {
+            //     notification.style.display = "none";
+            //     localStorage.setItem("notificationClicked", "true");
+            // })
+
             async function fetchExpiredCounts() {
                 try {
                     let highschoolResponse = await fetch("{{ route('checkExpiredMembers') }}");
--- resources/views/memberOption/index.blade.php
+++ resources/views/memberOption/index.blade.php
@@ -1,23 +1,26 @@
-@extends('layouts.templates.att.master')
+{{-- @extends('layouts.templates.att.master')
 @push('CSS')
 @endpush
 
 @section('Content')
-    <div class="flex justify-center mt-12 gap-3">
-        <a href="{{ url('/member/' . $id)}}">
-            <div class="user-detail bg-green-500 text-white hover:bg-green-400 w-[250px] h-[50px] text-center pt-3 rounded-xl">
-                <p>សាលាបត្រព័ត៌មានផ្ទាល់ខ្លួន</p>
-            </div>
-        </a>
-        <a href="{{ url('/member/request/' . $id)}}">
-            <div class="request-form bg-orange-400 text-white hover:bg-orange-300 w-[250px] h-[50px] text-center pt-3 rounded-xl">
-                <p>សំណើរសុំផ្ទេរជីវភាព</p>
-            </div>
-        </a>
-        <a href="{{ url('/member/card/' . $id)}}">
-            <div class="card bg-blue-400 text-white hover:bg-blue-300 w-[250px] h-[50px] text-center pt-3 rounded-xl">
-                <p>ប័ណ្ណសម្គាល់ខ្លួនយុវជន</p>
-            </div>
-        </a>
-    </div>
-@endsection
+<div class="flex justify-center mt-12 gap-3">
+    <a href="{{ url('/member/' . $id)}}">
+        <div
+            class="user-detail bg-green-500 font-siemreap text-white hover:bg-green-400 w-[250px] h-[50px] text-center pt-3 rounded-sm">
+            <p>សាលាបត្រព័ត៌មានផ្ទាល់ខ្លួន</p>
+        </div>
+    </a>
+    <a href="{{ url('/member/request/' . $id)}}">
+        <div
+            class="request-form bg-orange-400 font-siemreap text-white hover:bg-orange-300 w-[250px] h-[50px] text-center pt-3 rounded-sm">
+            <p>សំណើរសុំផ្ទេរជីវភាព</p>
+        </div>
+    </a>
+    <a href="{{ url('/member/card/' . $id)}}">
+        <div
+            class="card bg-blue-400 text-white font-siemreap hover:bg-blue-300 w-[250px] h-[50px] text-center pt-3 rounded-sm">
+            <p>ប័ណ្ណសម្គាល់ខ្លួនយុវជន</p>
+        </div>
+    </a>
+</div>
+@endsection --}}
