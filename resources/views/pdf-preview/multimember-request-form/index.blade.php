82f65f2399ad55812a25491d1cbe56b6ec8b6fb0
samotkhut-git <khotsamot99@gmail.com>
2025-05-07 14:09:31 +0700

    completed-pdf

--- resources/views/pdf-preview/multimember-request-form/index.blade.php
+++ resources/views/pdf-preview/multimember-request-form/index.blade.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Battambang:wght@100;300;400;700;900&family=Nunito:wght@700&display=swap"
+        rel="stylesheet">
     <title>Document</title>
     <style>
         .title1 {
