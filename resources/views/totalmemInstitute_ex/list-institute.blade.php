01ee9d1791bb30aca01f35991140b851e85117ee
viseth <simviseth509@gmail.com>
2025-08-13 22:32:46 +0700

    change style title

--- resources/views/totalmemInstitute_ex/list-institute.blade.php
+++ resources/views/totalmemInstitute_ex/list-institute.blade.php
@@ -4,7 +4,7 @@
 
 @section('Content')
     <div class="bg-[#fff] p-8 rounded-lg max-w-1000px m-5 shadow-md font-siemreap">
-        <h2 class="text-2xl font-medium text-center font-battambang my-2 pb-3">គ្រឹះស្ថានឧត្តមសិក្សា កាកបាទក្រហមកម្ពុជា 25
+        <h2 class="text-2xl text-center text-blue-600 font-koulen my-2 pb-3">គ្រឹះស្ថានឧត្តមសិក្សា កាកបាទក្រហមកម្ពុជា 25
             រាជធានី-​ខេត្ត</h2>
         {{-- search bar --}}
         <div class="filter_institute flex justify-end space-x-2 mt-12 mb-5">
@@ -32,26 +32,26 @@
             ul.empty();
             data.forEach((item) => {
                 ul.append(`
-                                        <li class="border-b bg-slate-50 rounded-lg hover:bg-indigo-50 p-2 hover:ring-indigo-200 hover:rounded-lg my-2">
-                                            <a href="/list-institute/${item.bhei_id}">
-                                                <div class="flex justify-between items-center">
-                                                    <div class="flex items-center">
-                                                        <img
-                                                            src="${item.image}"
-                                                            alt="Logo 1"
-                                                            class="ml-10 w-16 mr-8 rounded-full object-cover h-16"
-                                                        />
-                                                        <span class="text-lg font-battambang">${item.institute_kh}</span>
+                                            <li class="border-b bg-slate-50 rounded-lg hover:bg-indigo-50 p-2 hover:ring-indigo-200 hover:rounded-lg my-2">
+                                                <a href="/list-institute/${item.bhei_id}">
+                                                    <div class="flex justify-between items-center">
+                                                        <div class="flex items-center">
+                                                            <img
+                                                                src="${item.image}"
+                                                                alt="Logo 1"
+                                                                class="ml-10 w-16 mr-8 rounded-full object-cover h-16"
+                                                            />
+                                                            <span class="text-lg font-battambang">${item.institute_kh}</span>
+                                                        </div>
+                                                        <div class="grid grid-rows-2 m-2 place-items-end content-between gap-8">
+                                                            <span class="text-xs font-battambang">
+                                                                ស.ម <strong>${item.total_members ?? 0} នាក់</strong>
+                                                            </span>
+                                                        </div>
                                                     </div>
-                                                    <div class="grid grid-rows-2 m-2 place-items-end content-between gap-8">
-                                                        <span class="text-xs font-battambang">
-                                                            ស.ម <strong>${item.total_members ?? 0} នាក់</strong>
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            </a>
-                                        </li>
-                                    `)
+                                                </a>
+                                            </li>
+                                        `)
             })
         }
 
