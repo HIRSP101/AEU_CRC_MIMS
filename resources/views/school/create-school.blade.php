ae5ff42f63ea9ab4bd23127ecd97035d252b5d66
viseth <simviseth509@gmail.com>
2025-08-13 22:25:55 +0700

    modify style

--- resources/views/school/create-school.blade.php
+++ resources/views/school/create-school.blade.php
@@ -8,7 +8,7 @@
     @endphp
     <div class="flex justify-center items-center bg-gray-100">
         <div class="bg-white px-[10%] py-[5%] rounded-lg shadow-md w-[97%] mt-5">
-            <div class="text-center text-2xl font-bold mb-6 font-siemreap">
+            <div class="text-center text-2xl font-koulen mb-6 text-blue-600">
                 <h1>បង្កើតសាលារៀន​</h1>
             </div>
 
